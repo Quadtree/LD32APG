3a46d83f47c3f7d4d1ae6451479c390b184cc228
Eric Brown <quadtree@gmail.com>
2015-04-18 17:26:53 -0700

    Wheel adjustments

---
+++ Source/LD32APG/BuggyWheel.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/StaticMeshComponent.h"
+#include "BuggyWheel.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup = (Custom), BlueprintType, Blueprintable, meta = (BlueprintSpawnableComponent))
+class LD32APG_API UBuggyWheel : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
